54960cd7324fb297ce1a4349f8eb92572c4c469e
Dan Reardon <reardon.danielj@gmail.com>
2018-04-09 16:08:23 -0400

    playing w components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state text'
+    }
+    this.update = this.update.bind(this);
+  }
+
+  update(ev) {
+    this.setState({
+      txt: ev.target.value
+    })
+  }
+
+  render() {
+    const { txt } = this.state;
+    const { update } = this;
+    return (
+      <div>
+        <h1>Hello, Dan</h1>
+        <h2>{ txt }</h2>
+        <Widget update={ update } />
+        <Widget update={ update } />
+        <Widget update={ update } />
+      </div>
+    )
+  }
+}
+
+const Widget = (props) => {
+  const { update } = props;
+  return (
+    <input type="text" onChange={ update }></input>
+  )
+}
 
 export default App;
